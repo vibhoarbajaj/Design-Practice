e39a99b77367ecb7642ed3356fcb9521e5a87845
vibhoarbajaj <vibhoar2001@gmail.com>
2026-06-14 00:57:56 +0530

    self carrental got completed

--- lld/src/main/java/com/main/desgins/carrental/self/models/User.java
+++ lld/src/main/java/com/main/desgins/carrental/self/models/User.java
@@ -4,12 +4,10 @@ import com.main.desgins.carrental.self.services.Reservation;
 import lombok.Data;
 
 
-
 @Data
 public class User {
     private String username;
     private RentalVehicle rentalVehicle;
-    private Bill bill;
     private Location location;
     private Reservation reservations;
 }
