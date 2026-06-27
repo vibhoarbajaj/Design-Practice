9721fd755b3aa4b0f4d620a80a1ace8130cc6ae7
vibhoarbajaj <vibhoar2001@gmail.com>
2026-06-28 00:00:59 +0530

    course carrental got started with

---
+++ lld/src/main/java/com/main/desgins/carrental/course/CarRentalSystem.java
@@ -0,0 +1,14 @@
+package com.main.desgins.carrental.course;
+
+import com.main.desgins.carrental.course.models.User;
+import com.main.desgins.carrental.self.models.Store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CarRentalSystem {
+    private final List<Store> stores= new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
+
+}
